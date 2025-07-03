5807817e44a65ac40b20cd4e9c3b263a76a4614e
Leoni Cesar <cesar.leoni@softwire.com>
2025-07-03 16:44:09 +0300

    Done merging. MERGE!

--- src/fibonacci.ts
+++ src/fibonacci.ts
@@ -1,20 +1,26 @@
 export const computeFibonacciNumber = (position: number, recursion: boolean = false): number => {
+
     let notNullPosition = position;
+
     if (notNullPosition === null) {
         notNullPosition = 1;   
     }
+
     if (recursion) {
         if(notNullPosition>0)
             return recursiveFibonacci(1, 1, notNullPosition - 2);
         else
             return recursiveFibonacciNeg(position);
     }
+
     if (notNullPosition === 0) {
         return 0;
     }
+
     if (notNullPosition < 0) {
         return computeNegativeFibonacci(notNullPosition);
     }
+
     if (notNullPosition <= 2) {
         return 1;
     }
@@ -33,36 +39,47 @@ export const computeFibonacciNumber = (position: number, recursion: boolean = fa
     return largeFibonacciNumber;
 };
 
+
 const computeNegativeFibonacci = (position: number): number => {
+
     if (position >= 0) {
         throw new Error(`Position must be less than zero! Received: ${position}.`);
     }
+
     const resultIsNegative = position % 2 === 0;
     const absoluteResult = computeFibonacciNumber(-position);
     return resultIsNegative ? absoluteResult * -1 : absoluteResult;
 }
 
+
 export const computeFibonacciArray = (start: number, endInclusive: number): number[] => {
+
     const inputArray = [...Array(endInclusive - start + 1).keys()].map(i => i + start);
     return inputArray.map(x => computeFibonacciNumber(x));
+
 }
 
 const recursiveFibonacci = (previous: number, current: number, stepsLeft: number): number => {
+
     if (stepsLeft < 0) {
         return 1;
     }
+
     switch (stepsLeft) {
         case 0:
             return current;
         default:
             return recursiveFibonacci(current, previous + current, stepsLeft - 1);
     }
+
 }
 
 const recursiveFibonacciNeg = (initialPosition: number, left: number = 0, right: number = 1, position?: number): number => {
+
     const currentPosition = position ?? initialPosition;
     if (initialPosition === 0) return 0;
     if (currentPosition === 0) return left;
+
     if (initialPosition > 0) {
         return recursiveFibonacciNeg(initialPosition, right, left + right, currentPosition - 1);
     } else {
